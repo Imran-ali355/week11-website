a128024daa0fcd76ab5ab1a2e09e00d80d9ecc70
NASIR BLOCH <imranalik062@gmail.com>
2022-11-29 11:48:29 +0500

    first commit

---
+++ components/Card.js
@@ -0,0 +1,16 @@
+
+
+function Card() {
+  return (
+    <div >
+        <P>icon</P>
+        <p>Facebook</p>
+        <p>Fa</p>
+        <p>Facebook</p>
+
+
+    </div>
+  )
+}
+
+export default Card
